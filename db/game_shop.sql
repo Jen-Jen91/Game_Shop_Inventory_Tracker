9379a7591359048ffbe0ea639a20de966050f605
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-10-05 14:02:38 +0100

    Tables added: games, publishers

---
+++ db/game_shop.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS publishers;
+
+
+CREATE TABLE publishers(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE games(
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  description TEXT,
+  stock_quantity INT8,
+  buying_cost NUMERIC,
+  selling_price NUMERIC,
+  publisher_id INT8 REFERENCES publishers(id) ON DELETE CASCADE
+);
