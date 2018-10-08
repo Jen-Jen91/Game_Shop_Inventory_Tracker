e8bd3e591d8ad4e9abb136bf2626cc05744982ad
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-10-08 09:37:17 +0100

    Added Show for Games

--- controllers/games_controller.rb
+++ controllers/games_controller.rb
@@ -3,7 +3,14 @@ require("sinatra/contrib/all")
 require_relative("../models/game.rb")
 also_reload("../models/*")
 
+# INDEX
 get("/games") do
   @games = Game.all()
   erb(:"games/index")
+end
+
+# SHOW
+get("/games/:id") do
+  @game = Game.find(params[:id].to_i())
+  erb(:"games/show")
 end
