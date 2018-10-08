8b2a7a6067cddc3412ce32dd0df07c4fdf447db0
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-10-08 10:12:05 +0100

    Added New and Create for Games - problem with Show

--- controllers/games_controller.rb
+++ controllers/games_controller.rb
@@ -1,6 +1,7 @@
 require("sinatra")
 require("sinatra/contrib/all")
 require_relative("../models/game.rb")
+require_relative("../models/publisher.rb")
 also_reload("../models/*")
 
 # INDEX
@@ -10,7 +11,20 @@ get("/games") do
 end
 
 # SHOW
-get("/games/:id") do
-  @game = Game.find(params[:id].to_i())
-  erb(:"games/show")
+# get("/games/:id") do
+#   @game = Game.find(params[:id].to_i())
+#   erb(:"games/show")
+# end
+
+# NEW
+get("/games/new") do
+  @publishers = Publisher.all()
+  erb(:"games/new")
+end
+
+# CREATE
+post("/games") do
+  game = Game.new(params)
+  game.save
+  redirect to("/games")
 end
