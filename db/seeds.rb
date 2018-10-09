0550dbb01242ce83279b4a08a0a8aa5944311ad6
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-10-09 09:55:30 +0100

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,35 @@ Publisher.delete_all()
 
 
 publisher1 = Publisher.new({
-  "name" => "Bethesda",
-  "contact_name" => "Piper Wright",
-  "contact_email" => "piper.wright@email.com"
+  "name" => "Sony Interactive Entertainment",
+  "contact_name" => "Crash Bandicoot",
+  "contact_email" => "crash.bandicoot@email.com"
 })
 
 publisher1.save()
 
 publisher2 = Publisher.new({
-  "name" => "Sony",
-  "contact_name" => "Crash Bandicoot",
-  "contact_email" => "crash.bandicoot@email.com"
+  "name" => "Bethesda Softworks",
+  "contact_name" => "Preston Garvey",
+  "contact_email" => "preston.garvey@email.com"
 })
 
 publisher2.save()
 
+publisher3 = Publisher.new({
+  "name" => "Nintendo",
+  "contact_name" => "Preston Garvey",
+  "contact_email" => "preston.garvey@email.com"
+})
+
+publisher3.save()
+
 
 game1 = Game.new({
-  "title" => "Fallout 4",
-  "description" => "Apocalyptic action RPG",
-  "stock_quantity" => 10,
-  "buying_cost" => 30,
+  "title" => "Horizon Zero Dawn",
+  "description" => "Centuries after the world has fallen to animalistic machines, a young hunter named Aloy must travel the primitive land to learn the truth about her mysterious past and how this world came to be.",
+  "stock_quantity" => 20,
+  "buying_cost" => 25,
   "selling_price" => 45,
   "publisher_id" => publisher1.id
 })
@@ -35,16 +43,26 @@ game1 = Game.new({
 game1.save()
 
 game2 = Game.new({
-  "title" => "Journey",
-  "description" => "Adventure, Indie",
+  "title" => "Fallout 4",
+  "description" => "200 years after the nucleur bombs fell, after recently awakening from cryostasis, you must leave Vault 111 to explore a post-apocalyptic Boston and search for your kidnapped son",
   "stock_quantity" => 5,
-  "buying_cost" => 15,
-  "selling_price" => 25,
+  "buying_cost" => 20,
+  "selling_price" => 30,
   "publisher_id" => publisher2.id
 })
 
 game2.save()
 
+game3 = Game.new({
+  "title" => "The Legend of Zelda: Breath of the Wild",
+  "description" => "After awakening from a century-long sleep, Link must once again fight and defeat Ganon before he can destroy the kingdom of Hyrule.",
+  "stock_quantity" => 10,
+  "buying_cost" => 30,
+  "selling_price" => 45,
+  "publisher_id" => publisher3.id
+})
+
+game3.save()
 
 
 
